5c0728b73af5930987ab3fbe1754837c843d883f
truongminhquan <truongminhquan1999@gmail.com>
2022-01-27 15:37:52 +0700

    Update

--- Models/PrepDB.cs
+++ Models/PrepDB.cs
@@ -22,21 +22,24 @@ namespace weatherapi.Models
         {
             System.Console.WriteLine("Application Migration");
             context.Database.Migrate();
-
-            if (context.WeatherForecasts.Any())
+            System.Console.WriteLine("Done Migration");
+            System.Console.WriteLine(context.WeatherForecasts);
+            if (!context.WeatherForecasts.Any())
             {
                 System.Console.WriteLine("Adding data");
                 context.WeatherForecasts.AddRange(
-                    new WeatherForecast { Id = 0, TemperatureC = 10, Summary = "Cold" },
-                    new WeatherForecast { Id = 1, TemperatureC = 25, Summary = "Medium" },
-                    new WeatherForecast { Id = 2, TemperatureC = 40, Summary = "Hot" }
+                    new WeatherForecast { TemperatureC = 10, Summary = "Cold" },
+                    new WeatherForecast { TemperatureC = 25, Summary = "Medium" },
+                    new WeatherForecast { TemperatureC = 40, Summary = "Hot" }
                 );
+                context.SaveChanges();
                 System.Console.WriteLine("Done adding data");
             }
             else
             {
                 System.Console.WriteLine("Already have data");
             }
+
         }
     }
 }
